bad316ac546ffa828fe4707e6b3b645b6427b52a
zhenyangze <yangze.zhen@haochezhu.club>
2021-01-18 21:48:19 +0800

    Update index.php

--- examples/index.php
+++ examples/index.php
@@ -2,52 +2,33 @@
 
 require 'vendor/autoload.php';
 
-session_start();
+use Stevenmaguire\OAuth2\Client\Passport
 
-$provider = new Stevenmaguire\OAuth2\Client\Provider\Keycloak([
-    'authServerUrl'             => '',
-    'realm'                     => '',
-    'clientId'                  => '',
-    'clientSecret'              => '',
-    'redirectUri'               => '',
-    'encryptionAlgorithm'       => null,
-    'encryptionKey'             => null,
-    'encryptionKeyPath'         => null
+$passport = Passport::init([
+    'authServerUrl'         => 'http://xxxxx/auth',
+    'realm'                 => 'xxxxxxx',
+    'clientId'              => 'backend',
+    'clientSecret'          => '9030c395-1ffa-44ad-99ba-e2aba9586822',
+    'redirectUri'           => 'http://xxxx/',
 ]);
+//$passport->setModel(Passport::$MODEL_REFRESH_TOKEN);
 
-if (!isset($_GET['code'])) {
-    // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl();
-    $_SESSION['oauth2state'] = $provider->getState();
-    header('Location: '.$authUrl);
-    exit;
+//if ($passport->getAccessToken()) {
+    //$passport->logout();
+    //exit;
+//}
 
-// Check given state against previously stored one to mitigate CSRF attack
-} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-    unset($_SESSION['oauth2state']);
-    exit('Invalid state, make sure HTTP sessions are enabled.');
-} else {
-    // Try to get an access token (using the authorization coe grant)
-    try {
-        $token = $provider->getAccessToken('authorization_code', [
-            'code' => $_GET['code']
-        ]);
-    } catch (Exception $e) {
-        exit('Failed to get access token: '.$e->getMessage());
-    }
+$user = $passport->checkLogin();
 
-    // Optional: Now you have a token you can look up a users profile data
-    try {
 
-        // We got an access token, let's now get the user's details
-        $user = $provider->getResourceOwner($token);
-        // Use these details to create a new profile
-        printf('Hello %s!\n<br>', $user->getName());
+echo implode("<br>\n", [
+    $passport->getAccessToken(),
+    "--------------",
+    $passport->getLogoutUrl(),
+    $passport->getAuthorizationUrl(),
+    $user->getAttr('username'),
+]);
+echo "</pre>";
+print_r($passport->getToken());
+print_r($user->toArray());
 
-    } catch (Exception $e) {
-        exit('Failed to get resource owner: '.$e->getMessage());
-    }
-
-    // Use this to interact with an API on the users behalf
-    echo $token->getToken();
-}
