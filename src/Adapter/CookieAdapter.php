d3a08121e10c810c5c0f342f604b0da46d35d298
zhenyangze <zhenyangze@gmail.com>
2021-01-18 17:02:00 +0800

    add CookieAdapter

---
+++ src/Adapter/CookieAdapter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Stevenmaguire\OAuth2\Client\Adapter;
+
+/**
+ * Short description for DefaultAdapter.php
+ *
+ * @package CookieAdapter 
+ * @author zhenyangze <zhenyangze@gmail.com>
+ * @version 0.1
+ * @copyright (C) 2021 zhenyangze <zhenyangze@gmail.com>
+ * @license MIT
+ */
+
+class CookieAdapter extends AdapterAbstract
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getAccessToken()
+    {
+        return isset($_COOKIE['token']) ? $_COOKIE['token'] : '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function saveAccessToken($accessToken = '')
+    {
+        setcookie('token', $accessToken);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getToken($accessToken = '')
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function saveToken($accessToken = '', $token)
+    {
+        return;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCode()
+    {
+        return isset($_GET['code']) ? $_GET['code'] : '';
+    }
+}
